3c4d483e8cad047f61ee5a5a740a4a90fd08177b
Ignacio Torres Masdeu <i@itorres.net>
2016-06-27 01:05:39 +0200

    mount download volume

--- run.sh
+++ run.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 IMAGE_NAME=itorres/tv3dl
+COMMAND=/usr/src/app/tv3dl.py
+VOLUME=$(awk -F ': ' '/base:/ {print $2}' config.yaml)
 function exec_build {
     docker build -t itorres/tv3dl .
 }
 function exec_run {
-    docker run --rm -ti -v $(pwd):/usr/src/app ${IMAGE_NAME} bash
+    docker run --rm -ti -v $VOLUME:$VOLUME -v $(pwd):/usr/src/app ${IMAGE_NAME} $COMMAND
 }
 
-while getopts ":b" opt; do
+while getopts ":bc" opt; do
     case $opt in
-        b)
+        c)
             exec_build
+	    ;;
+	b)
+	    COMMAND=bash
+	    ;;
     esac
 done
 docker images | grep -q ${IMAGE_NAME} || exec_build
